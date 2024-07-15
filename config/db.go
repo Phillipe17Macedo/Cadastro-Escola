3eb537aeaf86e974c8c66546c3b2c5d555ca1f4b
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-07-15 19:45:41 -0300

    configurando-banco

--- config/db.go
+++ config/db.go
@@ -1,12 +1,18 @@
-package main
+package config
 
 import (
   "gorm.io/gorm"
 	"gorm.io/driver/postgres"
+  "log"
 )
   
+var DB *gorm.DB
+
+func Connect() {
   dsn := "user=postgres password=mysecretpassword dbname=mydb port=5432 sslmode=disable"
-  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+  var err error
+  DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
   if err != nil {
-	panic("failed to connect database")
+    log.Fatal("Failed to connect to database:", err)
   }
+}
