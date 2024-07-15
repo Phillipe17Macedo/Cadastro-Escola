a660f30fdd6102c5a88f319df3f9518a0ad0d137
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-07-15 19:53:43 -0300

    criacao-aluno-backend

--- models/aluno.go
+++ models/aluno.go
@@ -1,6 +1,8 @@
-type Aluno strut {
+package models
+
+type ALuno struct {
 	ID uint `gorm:"primaryKey"`
-	Nome string
-	Matricula string
-	CadTurma string
+	Nome string `gorm:"size:100"`
+	Matricula string `gorm:"unique"`
+	Turmas []Turma `gorm:"many2many:aluno_turmas;"`
 }
