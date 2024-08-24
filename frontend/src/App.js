d00565fe932c2358915babf56747c3942d08f384
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-24 16:17:08 -0300

    add-pagina-alunos-screen-home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Professores from './pages/professores';
 import Turmas from './pages/turmas';
+import Alunos from './pages/alunos';
 
 function App() {
   return (
@@ -21,6 +22,9 @@ function App() {
                 <li className="nav-item">
                   <Link className="nav-link" to="/turmas">Turmas</Link>
                 </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/alunos">Alunos</Link>
+                </li>
               </ul>
             </div>
           </div>
@@ -30,6 +34,7 @@ function App() {
           <Routes>
             <Route path="/professores" element={<Professores />} />
             <Route path="/turmas" element={<Turmas />} />
+            <Route path="/alunos" element={<Alunos />} />
             <Route path="/" element={<h2>Bem-vindo ao Cadastro-Escola</h2>} />
           </Routes>
         </div>
