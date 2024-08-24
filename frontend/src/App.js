f785177e8909d981e29224dfb64768d971169241
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-24 13:40:58 -0300

    add-logica-app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Professores from './pages/Professores';
+import Turmas from './pages/Turmas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/professores" element={<Professores />} />
+          <Route path="/turmas" element={<Turmas />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
