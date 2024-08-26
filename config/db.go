d652817e957c76bec9dbe4c2d0d3ebed050d7232
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-25 23:40:59 -0300

    ajustando-rota-mysql

--- config/db.go
+++ config/db.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 
 func Connect() {
 	// String de conexão com o MySQL
-	dsn := "root:teodoro10@tcp(127.0.0.1:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "admin:Teodoro10@tcp(127.0.0.1:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
 
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
