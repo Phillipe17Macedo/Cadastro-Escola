cd2dbc55c6923efc56f9f47403d730c3ae241bc0
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-30 15:51:11 -0300

    alt-cors-options

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	// Configurando CORS para permitir as origens específicas do frontend
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000", "https://cadastro-escola-production.up.railway.app"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
