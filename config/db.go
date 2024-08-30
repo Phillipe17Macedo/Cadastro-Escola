7a478e0d3b82f3c45cfa65868e7170cf266ff54c
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-30 10:57:39 -0300

    banco-railway

--- config/db.go
+++ config/db.go
@@ -1,4 +1,4 @@
-package config
+/*package config
 
 import (
 	"log"
@@ -22,4 +22,32 @@ func Connect() {
 
 	// AutoMigrate para sincronizar os modelos com o banco de dados
 	DB.AutoMigrate(&models.Professor{}, &models.Turma{}, &models.Aluno{}, &models.Atividade{}, &models.Nota{})
+}
+*/
+
+package config
+
+import (
+	"fmt"
+	"os"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("root"),
+		os.Getenv("mtaeXhHySpbVBwCoCswKiZAUSwoyacde"),
+		os.Getenv("mysql-zmno.railway.internal"),
+		os.Getenv("3306"),
+		os.Getenv("railway"),
+	)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database: " + err.Error())
+	}
 }
