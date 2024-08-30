01d1f7cad431cc9e41fe91bc4acdd5b438c71116
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-30 15:04:14 -0300

    ajuste-url-conexao-bd-railway

--- config/db.go
+++ config/db.go
@@ -1,8 +1,7 @@
-/*package config
+package config
 
 import (
 	"log"
-
 	"github.com/phillipe17macedo/Cadastro-Escola/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,43 +10,16 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	// String de conexão com o MySQL
-	dsn := "root:teodoro10@tcp(127.0.0.1:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
+	// URL completa fornecida pelo Railway
+	dsn := "root:ehrByRQgwxDRoiaAvYcuBCCxkTxwvKem@tcp(autorack.proxy.rlwy.net:26077)/railway?charset=utf8mb4&parseTime=True&loc=Local"
 
+	// Tenta abrir a conexão com o banco de dados
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 
-	// AutoMigrate para sincronizar os modelos com o banco de dados
+	// AutoMigrate para sincronizar os modelos com o banco de dados (opcional)
 	DB.AutoMigrate(&models.Professor{}, &models.Turma{}, &models.Aluno{}, &models.Atividade{}, &models.Nota{})
-}
-*/
-
-package config
-
-import (
-	"fmt"
-	"os"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-)
-
-var DB *gorm.DB
-
-func Connect() {
-	var err error
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-        os.Getenv("root"),
-        os.Getenv("mtaeXhHySpbVBwCoCswKiZAUSwoyacde"),
-        os.Getenv("mysql-zmno.railway.internal"),
-        os.Getenv("3306"),
-        os.Getenv("railway"),
-    )
-
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect to database: " + err.Error())
-	}
 }
