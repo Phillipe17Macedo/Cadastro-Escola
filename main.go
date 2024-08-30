dff57a0b28d122ea3ed0cd225e75bd9cb9b20566
Phillipe17Macedo <ph.stos.udia@gmail.com>
2024-08-29 23:18:58 -0300

    add-rotas-put-delete-metodos

--- main.go
+++ main.go
@@ -16,22 +16,32 @@ func main() {
 	// Métodos Rota Professor
 	r.GET("/professores", routes.GetProfessores)
 	r.POST("/professores", routes.CreateProfessor)
+	r.PUT("/professores/:id", routes.UpdateProfessor)
+	r.DELETE("/professores/:id", routes.DeleteProfessor)
 
 	// Métodos Rota Turma
 	r.GET("/turmas", routes.GetTurmas)
 	r.POST("/turmas", routes.CreateTurma)
+	r.PUT("/turmas/:id", routes.UpdateTurma)
+	r.DELETE("/turmas/:id", routes.DeleteTurma)
 
 	// Métodos Rota Aluno
 	r.GET("/alunos", routes.GetAlunos)
 	r.POST("/alunos", routes.CreateAluno)
+	r.PUT("/alunos/:id", routes.UpdateAluno)
+	r.DELETE("/alunos/:id", routes.DeleteAluno)
 
 	// Métodos Rota Atividades
 	r.GET("/atividades", routes.GetAtividades)
 	r.POST("/atividades", routes.CreateAtividade)
+	r.PUT("/atividades/:id", routes.UpdateAtividade)
+	r.DELETE("/atividades/:id", routes.DeleteAtividade)
 
 	// Métodos Rota Notas
 	r.GET("/notas", routes.GetNotas)
 	r.POST("/notas", routes.CreateNota)
+	r.PUT("/notas/:id", routes.UpdateNota)
+	r.DELETE("/notas/:id", routes.DeleteNota)
 
 	r.Run(":8080")
 }
